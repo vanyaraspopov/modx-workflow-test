feb3bc825a594af9d48d1f930bb9051fc6438bc6
RV <vanyaraspopov@mail.ru>
2018-01-23 20:58:08 +0500

    Настройка switchContext

--- core/elements/common/plugins/switchContext.php
+++ core/elements/common/plugins/switchContext.php
@@ -9,10 +9,10 @@ if ($modx->context->get('key') == 'mgr') {
 }
 
 switch ($_SERVER['HTTP_HOST']) {
-    case 'dev.fenom-test':
+    case 'dev.ombiz.ru':
         $modx->switchContext('dev');
         break;
-    case 'fenom-test':
+    case 'ombiz.ru':
         break;
     default:
         $modx->log(modX::LOG_LEVEL_ERROR, 'Check this plugin! May be your headache grew up from here.');
